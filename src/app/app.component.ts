5a9fbd021346c00bf5c08a43f77adab8762747f5
Norbert Aluba Dela Pena <norbs.knight@gmail.com>
2024-07-09 14:59:57 +0800

    #2 - Add edit component and nav links

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { ContactListComponent } from './components/contact-list/contact-list.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ContactListComponent],
+  imports: [RouterOutlet, RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { ContactListComponent } from './components/contact-list/contact-list.component';
+import { ContactEditComponent } from './components/contact-edit/contact-edit.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'home', component: ContactListComponent,
+  },
+  {
+    path: 'add', component: ContactEditComponent,
+  },
+  {
+    path: '', component: ContactListComponent,
+  },
+];
