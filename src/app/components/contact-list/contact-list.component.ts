aa9319ad2dd70eb96d3d8062f6609402519924df
Norbert Aluba Dela Pena <norbs.knight@gmail.com>
2024-07-09 14:48:10 +0800

    Add contact-list module and temp usage in app root

--- src/app/components/contact-list/contact-list.component.ts
+++ src/app/components/contact-list/contact-list.component.ts
@@ -1,12 +1,72 @@
 import { Component } from '@angular/core';
+import { Contact } from '../../models/contact.models';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'song-contact-list',
   standalone: true,
-  imports: [],
+  imports: [DatePipe],
   templateUrl: './contact-list.component.html',
   styleUrl: './contact-list.component.css'
-})
+  })
 export class ContactListComponent {
+  contacts: Contact[];
 
+
+  constructor() {
+    this.contacts = [
+      {
+        firstName: 'Norbert',
+        lastName: 'Dela Pena',
+        birthday: new Date(),
+        phone: {
+          phoneNumber: 1234423,
+          phoneType: 'Work',
+        },
+        address: {
+          houseNumber: '445',
+          street: 'AS Fortuna',
+          barangay: 'Banilad',
+          city: 'Mandaue city',
+          province: 'Cebu',
+          postalCode: 6014,
+        }
+      },
+      {
+        firstName: 'Norbs',
+        lastName: 'Dela Pena',
+        birthday: new Date(),
+        phone: {
+          phoneNumber: 123213,
+          phoneType: 'Work',
+        },
+        address: {
+          houseNumber: '645',
+          street: 'Salvador',
+          barangay: 'Labangon',
+          city: 'Cebu city',
+          province: 'Cebu',
+          postalCode: 6014,
+        }
+      },
+      {
+        firstName: 'Aston',
+        lastName: 'Cano',
+        birthday: new Date(),
+        phone: {
+          phoneNumber: 273482,
+          phoneType: 'Home',
+        },
+        address: {
+          houseNumber: 'IL121',
+          street: 'SE Llenes',
+          barangay: 'Paknaan',
+          city: 'Mandaue',
+          province: 'Cebu',
+          postalCode: 6014,
+        }
+      },
+ 
+    ]
+  }
 }
---
+++ src/app/models/contact.models.ts
@@ -0,0 +1,21 @@
+export interface Contact {
+  firstName: string,
+  lastName: string,
+  birthday: Date | null,
+  phone: Phone,
+  address: Address
+}
+
+export interface Phone {
+  phoneNumber: number,
+  phoneType: string
+}
+
+export interface Address {
+  houseNumber: string,
+  street: string,
+  barangay: string,
+  city: string,
+  province: string,
+  postalCode: number,
+}
