33180799ee6fac0915f2adda0f01bae36fa64cba
Norbert Aluba Dela Pena <norbs.knight@gmail.com>
2024-07-10 14:34:02 +0800

    Add form constants

---
+++ src/app/constants/data.ts
@@ -0,0 +1,34 @@
+export const phoneTypes = [
+  { title: 'Home', value: 'home' },
+  { title: 'Work', value: 'work' },
+];
+
+export const Provinces = [
+  { title: 'Cebu', value: 'cebu' },
+  { title: 'Manila', value: 'manila' },  
+  { title: 'Negros', value: 'negros' },  
+];
+
+export const Cities = {
+  cebu: [
+    { title: 'Cebu city', value: 'cebu' },
+    { title: 'Mandaue city', value: 'mandaue' },
+    { title: 'Lapu-Laput city', value: 'lapu-lapu' },
+    { title: 'Talisay city', value: 'talisay' },
+  ],
+  manila: [
+    { title: 'Malabon city', value: 'malabon' },
+    { title: 'Makati city', value: 'makati' },
+  ],
+};
+
+export const Barangays = {
+  cebu: [
+    { title: 'Apas', value: 'apas' },
+    { title: 'Labangon', value: 'labangon' },
+  ],
+  mandaue: [
+    { title: 'Bakilid', value: 'bakilid' },
+    { title: 'Banilad', value: 'banilad' },
+  ],
+};
