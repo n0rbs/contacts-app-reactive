ae95f1ea87ff868f4252a6a963269d4aea2457d6
Norbert Aluba Dela Pena <norbs.knight@gmail.com>
2024-07-10 08:39:25 +0800

    Add initial add/edit contact form

--- src/app/components/contact-edit/contact-edit.component.ts
+++ src/app/components/contact-edit/contact-edit.component.ts
@@ -1,12 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ReactiveFormsModule, FormBuilder, Validators } from '@angular/forms';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 @Component({
   selector: 'song-contact-edit',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './contact-edit.component.html',
   styleUrl: './contact-edit.component.css'
 })
-export class ContactEditComponent {
+export class ContactEditComponent implements OnInit {
+  contactForm = this.formBuilder.group({
+    firstName: ['', Validators.required],
+    lastName: ['', Validators.required],
+    birthday: ['', Validators.required],
+    phone: this.formBuilder.group({
+      phoneNumber: ['', Validators.required],
+      phoneType: ['', Validators.required],
+    }),
+    address: this.formBuilder.group({
+      houseNumber: ['', Validators.required],
+      street: ['', Validators.required],
+      barangay: ['', Validators.required],
+      city: ['', Validators.required],
+      province: ['', Validators.required],
+      postalCode: ['', Validators.required],
+    }),
+  });
 
+  constructor(private formBuilder: FormBuilder) {
+  }
+  ngOnInit() {
+  }
+
+  saveContact() {
+    console.log(this.contactForm.controls.phone);
+  }
 }
