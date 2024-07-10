84491f49e685f7154c37eafcdf48f41d4d578755
Norbert Aluba Dela Pena <norbs.knight@gmail.com>
2024-07-10 14:33:34 +0800

    #5 - Add initial reactive validator for a few input boxes

--- src/app/components/contact-edit/contact-edit.component.ts
+++ src/app/components/contact-edit/contact-edit.component.ts
@@ -1,18 +1,29 @@
+import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { ReactiveFormsModule, FormBuilder, Validators } from '@angular/forms';
 import { provideAnimations } from '@angular/platform-browser/animations';
 
+import { Barangays, Cities, Provinces } from '../../constants/data';
+
 @Component({
   selector: 'song-contact-edit',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './contact-edit.component.html',
   styleUrl: './contact-edit.component.css'
 })
 export class ContactEditComponent implements OnInit {
+  provinces = Provinces;
+  cities = Cities;
+  barangays = Barangays;
   contactForm = this.formBuilder.group({
-    firstName: ['', Validators.required],
-    lastName: ['', Validators.required],
+    firstName: ['', [
+      Validators.required,
+      Validators.minLength(5),
+    ]],
+    lastName: ['', [
+      Validators.required
+    ]],
     birthday: ['', Validators.required],
     phone: this.formBuilder.group({
       phoneNumber: ['', Validators.required],
@@ -28,12 +39,18 @@ export class ContactEditComponent implements OnInit {
     }),
   });
 
-  constructor(private formBuilder: FormBuilder) {
-  }
+  constructor(private formBuilder: FormBuilder) {}
+
   ngOnInit() {
   }
 
+  get firstName() {
+    return this.contactForm.controls.firstName;
+  }
+  get lastName() {
+    return this.contactForm.controls.lastName;
+  }
+
   saveContact() {
-    console.log(this.contactForm.controls.phone);
   }
 }
