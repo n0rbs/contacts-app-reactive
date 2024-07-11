8c44e9058cd6faea5965971cff2e33dd14bdb146
Norbert Aluba Dela Pena <norbs.knight@gmail.com>
2024-07-11 14:38:59 +0800

    Move test data to constants file and imported contact list

--- src/app/components/contact-list/contact-list.component.ts
+++ src/app/components/contact-list/contact-list.component.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
-import { Contact } from '../../models/contact.models';
 import { DatePipe } from '@angular/common';
 import { RouterLink } from '@angular/router';
+
+import { Contacts } from '../../constants/data';
+import { Contact } from '../../models/contact.models';
 
 @Component({
   selector: 'song-contact-list',
@@ -15,62 +17,6 @@ export class ContactListComponent {
 
 
   constructor() {
-    this.contacts = [
-      {
-        id: 1,
-        firstName: 'Norbert',
-        lastName: 'Dela Pena',
-        birthday: new Date(),
-        phone: {
-          phoneNumber: 1234423,
-          phoneType: 'Work',
-        },
-        address: {
-          houseNumber: '445',
-          street: 'AS Fortuna',
-          barangay: 'Banilad',
-          city: 'Mandaue city',
-          province: 'Cebu',
-          postalCode: 6014,
-        }
-      },
-      {
-        id: 2,
-        firstName: 'Norbs',
-        lastName: 'Dela Pena',
-        birthday: new Date(),
-        phone: {
-          phoneNumber: 123213,
-          phoneType: 'Work',
-        },
-        address: {
-          houseNumber: '645',
-          street: 'Salvador',
-          barangay: 'Labangon',
-          city: 'Cebu city',
-          province: 'Cebu',
-          postalCode: 6014,
-        }
-      },
-      {
-        id: 3,
-        firstName: 'Aston',
-        lastName: 'Cano',
-        birthday: new Date(),
-        phone: {
-          phoneNumber: 273482,
-          phoneType: 'Home',
-        },
-        address: {
-          houseNumber: 'IL121',
-          street: 'SE Llenes',
-          barangay: 'Paknaan',
-          city: 'Mandaue',
-          province: 'Cebu',
-          postalCode: 6014,
-        }
-      },
- 
-    ]
+    this.contacts = Contacts;
   }
 }
