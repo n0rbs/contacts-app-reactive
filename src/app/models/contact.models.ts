30eee9afc306513274a9b3c8b3bdbea19596d4f9
Norbert Aluba Dela Pena <norbs.knight@gmail.com>
2024-07-12 17:02:23 +0800

    Restore type definition for some optional properties

--- src/app/models/contact.models.ts
+++ src/app/models/contact.models.ts
@@ -2,7 +2,7 @@ export interface Contact {
   id: number,
   firstName: string,
   lastName: string,
-  birthday: Date,
+  birthday: Date | null,
   phone: Phone,
   address: Address
 }
@@ -13,8 +13,8 @@ export interface Phone {
 }
 
 export interface Address {
-  houseNumber: string,
-  street: string,
+  houseNumber: string | null,
+  street: string | null,
   barangay: string,
   city: string,
   province: string,
